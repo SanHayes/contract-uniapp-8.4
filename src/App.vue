a4efefcdfb19e5c968a14aa9605fd49b3f640270
Dou <mj568226@gmail.com>
2023-06-12 22:58:02 +0800

    默认显示英语

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <script>
 	export default {
 		onLaunch: function() {
-			console.log('App Launch')
+			const lang = uni.getLocale()
+			const langs = ['en', 'es', 'hi', 'IN', 'ja', 'vi', 'zh-TW'];
+			if (!langs.includes(lang)) {
+				uni.setLocale('en'),
+					this.$i18n.locale = 'en'
+			}
 		},
 		onShow: function() {
 			console.log('App Show')
