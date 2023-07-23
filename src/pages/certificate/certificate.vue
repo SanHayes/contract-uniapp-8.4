303cd83398e90be755cf58dc66b70b0f0d240f47
Dou <mj568226@gmail.com>
2023-07-23 17:47:13 +0800

    白皮书图文

--- src/pages/certificate/certificate.vue
+++ src/pages/certificate/certificate.vue
@@ -13,37 +13,37 @@
 				MSB Licence
 			</view>
 			<view class="paragraph">
-				The core members of Ethernet are from top Internet and financial companies. The platform has a professional
-				financial-grade transaction structure and a self-developed high-concurrency memory matching transaction engine.
-        Ethernet Community Co., Ltd. has obtained the MSB compliance operation license in the United States and Canada.
+        The core members of Ether EX are from top Internet and financial companies. The platform
+        has a professional financial-grade transaction structure and a self-developed high-concurrency memory matching transaction engine. Ethernet Community Co., Ltd. has obtained the
+        MSB compliance operation license in the United States and Canada.
 			</view>
 			<view class="sub-title">US MSB license</view>
 			<image class="img" src="../../static/img/certificate/msb_1.jpg" style="height: calc((100vw - 20px)*1280/960);"
 				mode="aspectFit"></image>
 			<view class="paragraph">
-				The MSB (MoneyServiceBusiness) license is a financial license regulated by the U.S. Treasury Department and
-				issued by the U.S. Treasury Department. It mainly regulates money service-related businesses and companies,
-				including digital currency, virtual currency transactions, and international payment companies. Companies
-				engaged in the above-mentioned related businesses in the United States must apply for an MSB license to operate
-				legally.
+        The MSB (MoneyServiceBusiness) license is a financial license regulated by the U.S. Treasury
+        Department and issued by the U.S. Treasury Department. It mainly regulates money
+        service-related businesses and companies, including digital currency, virtual currency transactions, and international payment companies. Companies engaged in the above-mentioned
+        related businesses in the United States must apply for an MSB license to operate legally.
 			</view>
 			<view class="sub-title">
 				Canadian MSB license
 			</view>
 			<image class="img" src="../../static/img/certificate/msb_2.jpg" mode="aspectFit"></image>
 			<view class="paragraph">
-				FINTRAC Canada, full name FTR Canada or MSB Canada, is a Canadian financial regulator established in 2000 to
-				collect, analyze, evaluate and disclose information about Canada's financial sector to protect Canada's
-				financial stability and security. FINTRAC is responsible for collecting, analyzing, evaluating and disclosing
-				information related to Canada's financial industry to protect Canada's financial stability and security.
-			</view>w
+        FINTRAC Canada, full name FTR Canada or MSB Canada, is a Canadian financial regulator
+        established in 2000 to collect, analyze, evaluate and disclose information about Canada's
+        financial sector to protect Canada's financial stability and security. FINTRAC is responsible
+        for collecting, analyzing, evaluating and disclosing information related to Canada's financial
+        industry to protect Canada's financial stability and security.
+			</view>
 			<view class="sub-title">
 				US stock license
 			</view>
 			<image class="img" src="../../static/img/certificate/msb_3.jpg" mode="aspectFit"></image>
 			<view class="sub-title">Canadian stock license</view>
 			<image class="img" src="../../static/img/certificate/msb_4.jpg" mode="aspectFit"></image>
-			<view class="paragraph">The platform has successfully obtained dual stock licenses in the United States and
+		<!--	<view class="paragraph">The platform has successfully obtained dual stock licenses in the United States and
 				Canada. Ethernet Community always regards operational compliance, fast transaction, privacy and security as an
 				important strategic direction for corporate development. The successful acquisition of dual-stock financial
 				licenses in the United States and Canada has become an important milestone in the standardized operation and
@@ -51,20 +51,20 @@
 			<view class="paragraph">Ethernet Community always adheres to the user first, and is committed to providing global users
 				with the most efficient, safe and high privacy asset management and trading services. In the future, 088
 				Exchange will continue to implement the global strategic layout and adhere to compliant operation and
-				development.</view>
+				development.</view>-->
 		</view>
 		<view class="certificate content" v-show="showIndex === 1">
       <!--<iframe style="width: 100%;height: 100%" :src="pdfUrl2" frameborder="0"></iframe>-->
 			<view class="title">certificate</view>
 			<view class="sub-title">Compliance operation, safety and efficiency</view>
-			<view class="paragraph">Ethernet Community: Ethernet Community Co., Ltd., the company operates the website: www.088vip.cc and
-				related mobile applications (hereinafter referred to as "platform"), and based on this platform provides users
-				with financial transactions between digital currencies and related Serve.</view>
+			<view class="paragraph"> Ethereum community: Ethereum community Co., Ltd., the company operates the website:
+        https://ethereum.org/en/ and related mobile applications (hereinafter referred to as "platform"), and based on this platform provides users with financial transactions between digital
+        currencies and related Serve.</view>
 			<view class="sub-title">American company certificate</view>
 			<image class="img" src="../../static/img/certificate/msb1.jpg" style="height: calc((100vw - 20px)*1280/989);"
 				mode="aspectFit"></image>
-			<view class="sub-title">Company Name: Ethernet Community Co., Ltd.</view>
-			<view class="sub-title">Company Address: 1391 Speer Blvd #266, DENVER, CO 80204, United States</view>
+			<view class="sub-title">Company Name: Ethereum community Co., Ltd</view>
+			<view class="sub-title">Company Address: 1391 Speer Blvd #266, DENVER, CO 80204, United States </view>
 			<image class="img" src="../../static/img/certificate/msb2.jpg" mode="aspectFit"></image>
 			<view class="paragraph">
 				US company certificate official website query address:
